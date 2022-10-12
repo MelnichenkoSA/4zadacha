51f7ddf0ecd49e5ad01502663489beb586b87015
geral <geral@DESKTOP-S7VJ1C8>
2022-10-12 12:26:05 +0700

    Добавьте файлы проекта.

---
+++ 4zadacha/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] A = new int[100];
+int sum = 0;
+Random rnd = new Random();
+for (int i = 0; i < A.Length; i++)
+{
+    A[i] = rnd.Next(-1000,1000);
+}
+Console.WriteLine("Чётные: ");
+for (int i = 0; i < A.Length; i++)
+{
+    if (A[i]%2 == 0)
+    {
+        Console.Write(A[i] + ", ");
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Кратные 3: ");
+for (int i = 0; i < A.Length; i++)
+{
+    if(A[i]%3 == 1)
+    {
+        Console.Write(A[i] + ", ");
+    }
+}
+Console.WriteLine();
+for (int i = 0; i < A.Length; i++)
+{
+    if (A[i] < 0)
+    {
+        sum += A[i];
+    }
+}
+Console.WriteLine("Сумма отрицательных чисел: " + sum);
